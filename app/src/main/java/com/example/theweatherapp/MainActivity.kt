37552f64553713a0aec9c554bbd2044625909f88
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-21 10:01:48 -0400

    Implemented DEXTER to handle permissions -> onChecked and rational functions not implemented yet.

--- app/src/main/java/com/example/theweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/theweatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.theweatherapp
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
@@ -7,6 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +21,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         if (!isLocationEnabled()) {
-            Toast.makeText(this, "Your location provider is turned off. Please turn it on.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Your location provider is turned off. Please turn it on.",
+                Toast.LENGTH_SHORT
+            ).show()
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
+        } else {
+            Dexter.withContext(this)
+                .withPermissions(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()) {
+                            //requestLocationData()
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "You have denied location permission. Please allow it, it's mandatory.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        //showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
         }
     }
+
 
     private fun isLocationEnabled(): Boolean {
         // This provides access to the system location services.
